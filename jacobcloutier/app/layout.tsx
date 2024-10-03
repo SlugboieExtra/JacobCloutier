2fc1a3f9e7f53824021156f23c812cd33b5784f5
SlugboieExtra <98238319+SlugboieExtra@users.noreply.github.com>
2024-10-03 17:02:08 -0300

    Favicon replace

--- jacobcloutier/app/layout.tsx
+++ jacobcloutier/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,300&display=swap"
